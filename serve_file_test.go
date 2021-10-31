56928b52a8726eb300504cc366ee6c6e2bf60aeb
zenklot <zenklot11@gmail.com>
2021-10-31 14:16:42 +0700

    writing code with httpRouter serveFile

---
+++ serve_file_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServerFile(t *testing.T) {
+
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello HttpRouter", string(body))
+
+}
+
+func TestServerFileGoodBye(t *testing.T) {
+
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/goodbye.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Good Bye HttpRouter", string(body))
+
+}
